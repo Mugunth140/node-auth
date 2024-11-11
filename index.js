7134dcb0ffcc65f2abb60860164e70cc1c5a0ace
Mugunth140 <mugunth140@gmail.com>
2024-11-11 21:28:46 +0530

    rearranged files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("login");
 });
 
 app.get("/home",(req, res) => {
@@ -26,13 +26,34 @@ app.post("/signup", async (req, res) => {
             password: req.body.password
         }
         const existingUser = await db.findOne({email : data.email})
+
         if(existingUser){
-          res.send("<h1>User Already Existing</h1>")
+          res.status(300).send("<h1>User Already Exist's</h1>")
         }else{
           const userdata = await db.insertMany(data)
+          res.status(201).send("<h1>User Created sucessfully</h1>")
         }
         } catch (error) {
         console.log("Error while accessing database: ", error)
+        res.status(500).error("Internal Server error",error)
+
+    }
+})
+
+app.post('/login', async (req, res) => {
+    try {
+      const  data = {
+        email : req.body.email,
+        password : req.body.password
+      }
+       
+      const userEmail = await db.findOne({email : data.email})
+      const userPassword = await db.findOne({password : data.password})
+       
+      userEmail && userPassword ? res.send("home page").status(200) : res.send("Invalid Email Or Password").status(404)
+
+    } catch (err) {
+      console.log('Error while requesting to login: ',err)
     }
 })
 
